395181048ec6a59f58ae2818f5e429db51decfdb
Gabriela Bohorquez <gaby030300@hotmail.com>
2024-03-06 16:21:00 -0500

    [Feature] Animations Main Character - Player Controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D _rb;
 
-    [SerializeField] private Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
+    private Animator _animator;
+
+    private Vector2 _movement;
+
+    private const string Horizontal = "Horizontal";
+    private const string Vertical = "Vertical";
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+        _rb.velocity = _movement * _moveSpeed;
         
+        _animator.SetFloat(Horizontal, _movement.x);
+        _animator.SetFloat(Vertical, _movement.y);
     }
 }
